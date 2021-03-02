78b02e97e34ffe766aaf1366caea90b593a512e7
Daniel Vargas <dannyvargashb@gmail.com>
2021-03-02 12:45:26 -0800

    Lesson 3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.daniel-vargas.com/',
+            description: 'These socks feature a ribbed cushioned design with a Vans logo at the top, perfect for every occasion.80% Cotton, 18% Nylon, 2% Spandex.'
         }
     }
 })
